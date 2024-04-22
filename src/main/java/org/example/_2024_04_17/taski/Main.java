a27b41fc9f556668f6de70b4d082db5a95c99965
Hanna Krzh <anutkakrz@gmail.com>
2024-04-22 21:53:28 +0200

    выполнила все задачи. в последней есть ощущение, что нужно было решать через MapTree, но я поняла что таким способом еще не осилю точно

--- src/main/java/org/example/_2024_04_17/taski/Main.java
+++ src/main/java/org/example/_2024_04_17/taski/Main.java
@@ -18,24 +18,32 @@ public class Main {
         System.out.println(FigureTasks.findRectangleWithMaxArea(figures));
         FigureTasks.printEquilateralTriangles(figures);
         System.out.println("Сумма радиусов всех окружностей, заданного цвета: " +
-                FigureTasks.sumOfRadiiForColor(figures, Color.BLACK));
+                FigureTasks.sumOfRadiiForColor(figures, Color.BLACK)+" \n");
         System.out.println("Наличие треугольника и прямоугольника одного цвета: " +
-                FigureTasks.hasRectangleAndTriangleOfSameColor(figures));
+                FigureTasks.hasRectangleAndTriangleOfSameColor(figures)+" \n");
         System.out.println("круг с минимальным радиусом среди кругов заданного цвета: "+
-                FigureTasks.findCircleWithMinRadiusOfColor(figures, WHITE));
+                FigureTasks.findCircleWithMinRadiusOfColor(figures, WHITE)+" \n");
         System.out.println("кол-во треугольников у которых, хотя бы одна сторона больше заданного значения: "+
-                FigureTasks.countTrianglesWithSideGreaterThan (figures,15));
+                FigureTasks.countTrianglesWithSideGreaterThan (figures,15)+" \n");
         System.out.println("круги у которых радиус равен любой из сторон треугольника: "+
-                FigureTasks.findCirclesWithRadiusEqualToTriangleSide(figures));
+                FigureTasks.findCirclesWithRadiusEqualToTriangleSide(figures)+" \n");
         System.out.println("Треугольник у которого разница между максимальной стороной и минимальной стороной" +
-                " - минимальна: " +FigureTasks.findTriangleWithSmallestSideDifference(figures));
+                " - минимальна: " +FigureTasks.findTriangleWithSmallestSideDifference(figures)+" \n");
 //        FigureTasks.printFiguresWithAtLeastOneMatchingParameter(figures);
         System.out.println("Круги у которых радиус больше, чем сумма сторон треугольника: "
-                +FigureTasks.findCirclesWithRadiusGreaterThanSumOfAnyTriangleSides(figures));
+                +FigureTasks.findCirclesWithRadiusGreaterThanSumOfAnyTriangleSides(figures)+" \n");
         System.out.println("круг с наименьшим соотношением площади к радиусу: "
-        +FigureTasks.findCircleWithSmallestAreaToRadiusRatio(figures));
+        +FigureTasks.findCircleWithSmallestAreaToRadiusRatio(figures)+" \n");
         System.out.println("Прямоугольник с наибольшим соотношением диагонали к периметру: "+
-                FigureTasks.findRectangleWithLargestDiagonalToPerimeterRatio(figures));
+                FigureTasks.findRectangleWithLargestDiagonalToPerimeterRatio(figures)+" \n");
+        System.out.println("кол-во кругов по цветам: "+TasksMap.countCirclesOfColor(figures)+" \n");
+        System.out.println("Суммы радиусов по цветам: "+TasksMap.sumOfRadiiForColor(figures)+" \n");
+        System.out.println("Прямоугольник с максимальным соотношением диагонали к периметру: "+
+                TasksMap.findRectangleWithLargestDiagonalToPerimeterRatio(figures)+" \n");
+        System.out.println("Круги радиус который совпадает с любой из сторон треугольников: "+
+                TasksMap.findCirclesWithRadiusEqualToTriangleSide(figures)+" \n");
+        System.out.println("совпадение цветов у треугольников и прямоугольников: "+
+                TasksMap.hasRectangleAndTriangleOfSameColor(figures)+" \n");
     }
 }
 
